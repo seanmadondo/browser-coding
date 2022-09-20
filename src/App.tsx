d50311b9dab6b18c51d9babf9cd54440e3742a8f
Sean Madondo <seanmadondo@yahoo.com>
2022-09-20 15:27:38 +1200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/** @jsxImportSource @emotion/react */
 
-function App() {
+import { Link } from "react-router-dom";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <section
+      css={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <header>
+        <h1>My Browser Components</h1>
       </header>
-    </div>
+      <section
+        css={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+      >
+        <Link to="/faqs">FAQ</Link>
+        <Link to="/imageCarousel">Image Carousel</Link>
+        <Link to="/tree">Tree Visulisation component</Link>
+      </section>
+    </section>
   );
-}
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import FAQ from "./routes/faqs";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ImageCarousel from "./routes/carousel";
+import { Tree } from "./routes/tree";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/faqs" element={<FAQ />} />
+        <Route path="/imageCarousel" element={<ImageCarousel />} />
+        <Route path="/tree" element={<Tree />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
